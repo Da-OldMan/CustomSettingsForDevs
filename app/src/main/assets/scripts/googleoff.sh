7b124dcb46b406e9b677d0f03c9d4c39443f20be
Thomas Shimko <thomas.endo@gmail.com>
2016-01-14 19:03:15 +0200

    Change of string to prepare for 5.0 Changed code with new Observers in mind Eliminated old Aurora control app Still problems with access to phone codes.... Change all android:keys to reflect real db keys, now need to broadcast Added TWlauncher silent restart on Desktop Enhacements Commented out buggy menus & deprecated old app Olu added all scripts into "usefull scripts menu", Strings fixed Arrays.xml created for arrays.

---
+++ app/src/main/assets/scripts/googleoff.sh
@@ -0,0 +1,24 @@
+#!/system/bin/sh
+export PATH=/system/bin:$PATH
+
+mount -o rw,remount /system
+
+rm -rf /system/priv-app/GmsCore/
+rm -rf /system/priv-app/GoogleBackupTransport/
+rm -rf /system/priv-app/GoogleFeedback/
+rm -rf /system/priv-app/GoogleLoginService/
+rm -rf /system/priv-app/GoogleOneTimeInitializer/
+rm -rf /system/priv-app/GooglePartnerSetup/
+rm -rf /system/priv-app/GoogleServicesFramework/
+rm -rf /system/priv-app/Phonesky/
+rm -rf /system/framework/com.google.android.maps.jar
+rm -rf /system/framework/com.google.android.media.effects.jar
+rm -rf /system/app/Gmail2/
+rm -rf /system/app/GoogleCalendarSyncAdapter/
+rm -rf /system/app/GoogleContactsSyncAdapter/
+rm -rf /system/app/GoogleTTS/
+
+sleep 3
+
+reboot
+
